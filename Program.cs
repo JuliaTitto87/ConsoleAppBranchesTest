0ab0b6d60dce1d9a37f7caeb720c75bf9076ff2b
JuliaTitto87 <133301250+JuliaTitto87@users.noreply.github.com>
2024-01-05 14:28:09 +0300

    Added the second line

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppBranchesTest
         static void Main(string[] args)
         {
             Console.WriteLine("This is the first line");
+            Console.WriteLine("This is the second line");
         }
     }
 }
